3881c9f8e833aa153f00aa78ea0ba139152a4b14
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-04-06 12:11:46 +0300

    created chat window and styled

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,4 +1,14 @@
 import React, { useEffect, useState } from "react";
+import {
+  Text,
+  Container,
+  Input,
+  Button,
+  Flex,
+  Box,
+  Stack,
+} from "@chakra-ui/react";
+import { ArrowRightIcon } from "@chakra-ui/icons";
 
 const Chat = ({ socket, username, room }) => {
   const [currentMessage, setCurrentMessage] = useState("");
@@ -25,22 +35,51 @@ const Chat = ({ socket, username, room }) => {
   }, [socket]);
 
   return (
-    <div>
-      <div>Chat Header</div>
-      <div>
-        <p>Live Chat</p>
-      </div>
-      <div>Chat Body</div>
-      <div>Chat Footer</div>
-      <div>
-        <input
-          type="text"
-          placeholder="Hey..."
-          onChange={(e) => setCurrentMessage(e.target.value)}
-        />
-        <button onClick={sendMessage}>&#9658;</button>
-      </div>
-    </div>
+    <Flex>
+      <Stack
+        width="400px"
+        mt="20"
+        color="white"
+        border="2px"
+        borderColor="red.200"
+        borderRadius="lg"
+      >
+        {/* chat Header */}
+        <Box
+          maxW="md"
+          borderWidth="1px"
+          overflow="hidden"
+          alignItems="center"
+          backgroundColor="red"
+          padding="10px 20px"
+        >
+          <Text>Live Chat</Text>
+        </Box>
+        {/* Chat Body */}
+        <Box width="100%" height="300px"></Box>
+        {/* Chat Footer */}
+        <Box display="flex">
+          <Input
+            width="80%"
+            type="text"
+            placeholder="Hey..."
+            onChange={(e) => setCurrentMessage(e.target.value)}
+          />
+          <Button
+            width="20%"
+            onClick={sendMessage}
+            background="red.500"
+            _hover={{
+              background: "white",
+              color: "red.500",
+              border: "1px red solid",
+            }}
+          >
+            <ArrowRightIcon />
+          </Button>
+        </Box>
+      </Stack>
+    </Flex>
   );
 };
 
