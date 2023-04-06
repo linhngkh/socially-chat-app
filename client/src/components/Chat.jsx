7c5bf6f99fee2cf7f88fc7136d30dfec6cb3f408
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-04-06 17:00:33 +0300

    added scrollToView method for new mess coming smoothly

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
   Text,
   Input,
@@ -9,10 +9,20 @@ import {
   Container,
 } from "@chakra-ui/react";
 import { ArrowRightIcon } from "@chakra-ui/icons";
+import ScrollToBottom from "react-scroll-to-bottom";
+
+const ChatBody = {
+  width: "100%",
+  height: "100%",
+  overflowY: "scroll",
+  overflowX: "hidden",
+  borderBottom: "1px red solid",
+};
 
 const Chat = ({ socket, username, room }) => {
   const [currentMessage, setCurrentMessage] = useState("");
   const [messageList, setMessageList] = useState([]);
+  const scrollRef = useRef();
 
   const sendMessage = async () => {
     if (currentMessage !== "") {
@@ -26,6 +36,8 @@ const Chat = ({ socket, username, room }) => {
           new Date(Date.now()).getMinutes(),
       };
       await socket.emit("send_message", messageData);
+      setMessageList((list) => [...list, messageData]);
+      setCurrentMessage("");
     }
   };
   //listen to receive message event on socket server, side effects
@@ -34,6 +46,10 @@ const Chat = ({ socket, username, room }) => {
       setMessageList((list) => [...list, data]);
     });
   }, [socket]);
+  //use scrollIntoView method when a new message comes up
+  useEffect(() => {
+    scrollRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [currentMessage]);
 
   return (
     <Flex>
@@ -56,52 +72,55 @@ const Chat = ({ socket, username, room }) => {
           <Text color="white">Live Chat</Text>
         </Box>
         {/* Chat Body */}
-        <Box width="100%" height="300px" borderBottom="1px red solid">
-          {messageList.map((messageContent) => {
-            return (
-              <Container
-                key={messageContent.id}
-                id={username === messageContent.author ? "You" : "other"}
-              >
-                <Box>
-                  <Text>{messageContent.message}</Text>
-                </Box>
-                <Flex>
-                  <Text mr={3} fontWeight="bold">
-                    {messageContent.author}
-                  </Text>
-                  <Text>{messageContent.time}</Text>
-                </Flex>
-              </Container>
-            );
-          })}
-        </Box>
-        {/* Chat Footer */}
-        <Box display="flex">
-          <Input
-            variant="unstyled"
-            paddingLeft="5"
-            width="80%"
-            type="text"
-            placeholder="Hey..."
-            onChange={(e) => setCurrentMessage(e.target.value)}
-            onKeyPress={(e) => e.key === "Enter" && sendMessage()}
-          />
-          <Button
-            onClick={sendMessage}
-            background="red.500"
-            color="white"
-            width="20%"
-            _hover={{
-              background: "white",
-              color: "red.500",
-              border: "1px red solid",
-            }}
-          >
-            <ArrowRightIcon />
-          </Button>
+        <Box width="100%" height="300px">
+          <div ref={scrollRef}>
+            {messageList.map((messageContent) => {
+              return (
+                <Container
+                  key={messageContent.id}
+                  id={username === messageContent.author ? "you" : "other"}
+                >
+                  <Box>
+                    <Text>{messageContent.message}</Text>
+                  </Box>
+                  <Flex>
+                    <Text mr={3} fontWeight="bold">
+                      {messageContent.author}
+                    </Text>
+                    <Text>{messageContent.time}</Text>
+                  </Flex>
+                </Container>
+              );
+            })}
+          </div>
         </Box>
       </Stack>
+      {/* Chat Footer */}
+      <Box display="flex">
+        <Input
+          variant="unstyled"
+          paddingLeft="5"
+          width="80%"
+          type="text"
+          value={currentMessage}
+          placeholder="Hey..."
+          onChange={(e) => setCurrentMessage(e.target.value)}
+          onKeyPress={(e) => e.key === "Enter" && sendMessage()}
+        />
+        <Button
+          onClick={sendMessage}
+          background="red.500"
+          color="white"
+          width="20%"
+          _hover={{
+            background: "white",
+            color: "red.500",
+            border: "1px red solid",
+          }}
+        >
+          <ArrowRightIcon />
+        </Button>
+      </Box>
     </Flex>
   );
 };
