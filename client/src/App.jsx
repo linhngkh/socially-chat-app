f5a91c017a89ca8e61f80c23ad288b36a29f1c61
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-04-06 12:52:49 +0300

    mapping message content to show in chat window

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,52 +1,65 @@
 import { useState } from "react";
 import io from "socket.io-client";
 import Chat from "./components/Chat";
-import { Text, Container, Stack, Input, Button } from "@chakra-ui/react";
+import {
+  Text,
+  Container,
+  Stack,
+  Input,
+  Button,
+  VStack,
+} from "@chakra-ui/react";
 
 const socket = io.connect("http://localhost:3001");
 
 const boxStyles = {
   display: "flex",
-  flexDirection: "column",
   alignItems: "center",
   justifyContent: "center",
+  height: "100vh",
 };
 
 function App() {
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
 
   const joinRoom = () => {
     if (username !== "" && room !== "") {
       socket.emit("join_room", room);
+      setShowChat(true);
     }
   };
 
   return (
     <Container sx={boxStyles}>
-      <Text fontSize="5xl" color="red">
-        Join A Chat{" "}
-      </Text>
-      <Stack spacing={3}>
-        {" "}
-        <Input
-          variant="outline"
-          type="text"
-          placeholder="Join..."
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <Input
-          variant="outline"
-          type="text"
-          placeholder="Room ID"
-          onChange={(e) => setRoom(e.target.value)}
-        />
-        <Button colorScheme="red" onClick={joinRoom}>
-          Join A Room
-        </Button>
-      </Stack>
-
-      <Chat socket={socket} username={username} room={room} />
+      {!showChat ? (
+        <VStack>
+          <Text fontSize="5xl" color="red">
+            Join A Chat{" "}
+          </Text>
+          <Stack spacing={3}>
+            {" "}
+            <Input
+              variant="outline"
+              type="text"
+              placeholder="Join..."
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input
+              variant="outline"
+              type="text"
+              placeholder="Room ID"
+              onChange={(e) => setRoom(e.target.value)}
+            />
+            <Button colorScheme="red" onClick={joinRoom}>
+              Join A Room
+            </Button>
+          </Stack>
+        </VStack>
+      ) : (
+        <Chat socket={socket} username={username} room={room} />
+      )}
     </Container>
   );
 }
--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from "react";
 import {
   Text,
-  Container,
   Input,
   Button,
   Flex,
   Box,
   Stack,
+  Container,
 } from "@chakra-ui/react";
 import { ArrowRightIcon } from "@chakra-ui/icons";
 
 const Chat = ({ socket, username, room }) => {
   const [currentMessage, setCurrentMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
 
   const sendMessage = async () => {
     if (currentMessage !== "") {
@@ -30,7 +31,7 @@ const Chat = ({ socket, username, room }) => {
   //listen to receive message event on socket server, side effects
   useEffect(() => {
     socket.on("receive_message", (data) => {
-      console.log(data);
+      setMessageList((list) => [...list, data]);
     });
   }, [socket]);
 
@@ -38,11 +39,10 @@ const Chat = ({ socket, username, room }) => {
     <Flex>
       <Stack
         width="400px"
-        mt="20"
-        color="white"
         border="2px"
         borderColor="red.200"
         borderRadius="lg"
+        overflow="hidden"
       >
         {/* chat Header */}
         <Box
@@ -53,22 +53,42 @@ const Chat = ({ socket, username, room }) => {
           backgroundColor="red"
           padding="10px 20px"
         >
-          <Text>Live Chat</Text>
+          <Text color="white">Live Chat</Text>
         </Box>
         {/* Chat Body */}
-        <Box width="100%" height="300px"></Box>
+        <Box width="100%" height="300px" borderBottom="1px red solid">
+          {messageList.map((messageContent) => {
+            return (
+              <Container
+                key={messageContent.id}
+                id={username === messageContent.author ? "You" : "other"}
+              >
+                <Box>
+                  <Text>{messageContent.message}</Text>
+                </Box>
+                <Box>
+                  <Text>{messageContent.author}</Text>
+                  <Text>{messageContent.time}</Text>
+                </Box>
+              </Container>
+            );
+          })}
+        </Box>
         {/* Chat Footer */}
         <Box display="flex">
           <Input
+            variant="unstyled"
+            paddingLeft="5"
             width="80%"
             type="text"
             placeholder="Hey..."
             onChange={(e) => setCurrentMessage(e.target.value)}
           />
           <Button
-            width="20%"
             onClick={sendMessage}
             background="red.500"
+            color="white"
+            width="20%"
             _hover={{
               background: "white",
               color: "red.500",
